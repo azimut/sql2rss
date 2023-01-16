885f3bdfccad86e25cf1a183f90a838e4088d5dd
azimut <azimut.github@protonmail.com>
2023-01-15 23:25:09 -0300

    add channels map, to get channels links

--- src/rss.ml
+++ src/rss.ml
@@ -8,34 +8,26 @@ let link = "https://discord.com/channels/@me"
 
 let tag ?(attr = []) name data =
   let plist_to_string plist =
-    plist
-    |> List.map (fun (x, y) -> Printf.sprintf "%s=\"%s\"" x y)
-    |> String.concat " "
+    plist |> List.map (fun (x, y) -> Printf.sprintf "%s=\"%s\"" x y) |> String.concat " "
   in
   match attr with
   | [] -> Printf.printf "<%s>%s</%s>\n" name data name
-  | _ ->
-      Printf.printf "<%s %s>%s</%s>\n" name (plist_to_string attr) data name
+  | _ -> Printf.printf "<%s %s>%s</%s>\n" name (plist_to_string attr) data name
 
 let print_header () =
   tag "title" title ;
   tag "link" link ;
-  print_endline @@ "<atom:link href=\"" ^ link
-  ^ "\" rel=\"self\" type=\"application/rss+xml\" />" ;
+  print_endline @@ "<atom:link href=\"" ^ link ^ "\" rel=\"self\" type=\"application/rss+xml\" />" ;
   tag "lastBuildDate" @@ now ()
 
 let print_entry (entry : Sql.t) =
   let anchorify (s : string) =
     let mapanchor w =
-      if String.starts_with ~prefix:"http" w then
-        Printf.sprintf "<a href='%s'>%s</a>" w w
-      else w
+      if String.starts_with ~prefix:"http" w then Printf.sprintf "<a href='%s'>%s</a>" w w else w
     in
     s |> String.split_on_char ' ' |> List.map mapanchor |> String.concat " "
   in
-  let print_author () =
-    Printf.printf "<author><name>%s</name></author>\n" entry.window
-  in
+  let print_author () = Printf.printf "<author><name>%s</name></author>\n" entry.window in
   let ntobr s = s |> String.split_on_char '\n' |> String.concat " <br> " in
   let sub s n = String.sub s 0 @@ min n @@ String.length s in
   print_endline "<item>" ;
@@ -43,18 +35,15 @@ let print_entry (entry : Sql.t) =
   tag "title" @@ sub entry.message 80 ;
   tag "pubDate" @@ date entry.created_at ;
   tag "guid" ~attr:[("isPermaLink", "false")] @@ date entry.created_at ;
-  tag "link" link ~attr:[("href", link)] ;
-  tag "description"
-    (Printf.sprintf "<![CDATA[\n%s\n]]>\n"
-       (entry.message |> ntobr |> anchorify) ) ;
+  tag "link" entry.window ~attr:[("href", Sql.find_channel entry.window)] ;
+  tag "description" (Printf.sprintf "<![CDATA[\n%s\n]]>\n" (entry.message |> ntobr |> anchorify)) ;
   print_endline "</item>"
 
 let print (entries : Sql.t list) =
   print_endline "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" ;
   print_endline
     "<rss version=\"2.0\" xmlns:discourse=\"http://www.discourse.org/\" \
-     xmlns:atom=\"http://www.w3.org/2005/Atom\" \
-     xmlns:dc=\"http://purl.org/dc/elements/1.1/\">" ;
+     xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\">" ;
   print_endline "<channel>" ;
   print_header () ;
   List.iter print_entry entries ;
--- src/sql.ml
+++ src/sql.ml
@@ -1,10 +1,32 @@
-let database = "irclogs"
+type t = {created_at: Unix.tm; window: string; message: string}
 
-let user = "postgres"
+module Chans = Map.Make (String)
+
+let database_name = "irclogs"
+
+let database_user = "postgres"
+
+let channels_alist =
+  [ ("#avisos-laborales-deployar", "https://discord.com/channels/712042571245027438/726231396628168734")
+  ; ("#bolsa-de-trabajo-infochicas", "https://discord.com/channels/722479605814001766/722481989864652870")
+  ; ("#job-search-ekoparty", "https://discord.com/channels/727291328915832914/763602718668619786")
+  ; ("#ofertas-de-empleos", "https://discord.com/channels/768278151435386900/793661563705360394")
+  ; ("#ofertas-senior-leader", "https://discord.com/channels/594363964499165194/759056592648798229")
+  ; ("#ofertas-semi-senior", "https://discord.com/channels/594363964499165194/757967606815916174")
+  ; ("#ofertas-trainee", "https://discord.com/channels/594363964499165194/757967541170733257")
+  ; ("#ofertas-junior", "https://discord.com/channels/594363964499165194/757967571302482050") ]
+
+let channels_map = channels_alist |> List.to_seq |> Chans.of_seq
+
+let find_channel chan = Chans.find chan channels_map
 
 let query =
-  {|
-select extract(epoch from
+  channels_alist |> List.map fst
+  |> List.map (fun s -> "'" ^ s ^ "'")
+  |> String.concat ","
+  |> Printf.sprintf
+       {|
+ select extract(epoch from
          date_trunc('minute', created_at)) as dt,
        logs.window                         as win,
        regexp_replace(-- trim at the beggining
@@ -16,22 +38,12 @@ select extract(epoch from
                '\*\*.+\*\*', ''),
            chr(10) order by created_at), '^\s+', '')   as msg
   from logs
- where logs.window in ('#avisos-laborales-deployar',
-                       '#bolsa-de-trabajo-infochicas',
-                       '#dev-ofertas',
-                       '#job-search-ekoparty',
-                       '#ofertas-de-empleos',
-                       '#ofertas-senior-leader',
-                       '#ofertas-semi-senior',
-                       '#ofertas-trainee',
-                       '#ofertas-junior')
+ where logs.window in (%s)
    and created_at > current_date - interval '7 days'
  group by dt, win
  order by dt desc
  limit 20;
 |}
-
-type t = {created_at: Unix.tm; window: string; message: string}
 
 let to_record = function
   | [c; w; m] ->
@@ -42,7 +54,6 @@ let to_record = function
   | _ -> assert false
 
 let entries () =
-  Pgx_unix.with_conn ~ssl:`No ~database ~user (fun dbh ->
-      Pgx_unix.simple_query dbh query
-      |> List.hd |> List.map to_record
+  Pgx_unix.with_conn ~ssl:`No ~database:database_name ~user:database_user (fun dbh ->
+      Pgx_unix.simple_query dbh query |> List.hd |> List.map to_record
       |> List.filter (fun r -> r.message <> "") )
